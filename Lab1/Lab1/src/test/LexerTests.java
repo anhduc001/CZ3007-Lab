141afb1b6d34988256acb0812e6a58789186ff16
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2021-02-10 15:41:23 +0800

    add test for punctuation, operator, identifier

--- Lab1/Lab1/src/test/LexerTests.java
+++ Lab1/Lab1/src/test/LexerTests.java
@@ -56,21 +56,6 @@ public class LexerTests {
 	}
 
 	@Test
-	public void testStringLiteralWithDoubleQuote() {
-		runtest("\"\"\"",
-				new Token(STRING_LITERAL, 0, 0, ""),
-				(Token)null,
-				new Token(EOF, 0, 3, ""));
-	}
-
-	@Test
-	public void testStringLiteral() {
-		runtest("\"\\n\"", 
-				new Token(STRING_LITERAL, 0, 0, "\\n"),
-				new Token(EOF, 0, 4, ""));
-	}
-	
-	@Test
 	public void testKeywordAndIdentifier() {
 		runtest("boolean Boolean wHiLe while true True falSe false breaker integer",
 				new Token(BOOLEAN, 0, 0, "boolean"),
@@ -85,7 +70,117 @@ public class LexerTests {
 				new Token(ID, 0, 58, "integer"),
 				new Token(EOF, 0, 65, ""));
 	}
-	
+
+	@Test
+	public void testPunctuations() {
+		runtest(", [ { ( ] } ) ;",
+				new Token(COMMA, 0, 0, ","),
+				new Token(LBRACKET, 0, 2, "["),
+				new Token(LCURLY, 0, 4, "{"),
+				new Token(LPAREN, 0, 6, "("),
+				new Token(RBRACKET, 0 , 8, "]"),
+				new Token(RCURLY, 0, 10, "}"),
+				new Token(RPAREN, 0, 12, ")"),
+				new Token(SEMICOLON, 0, 14, ";"),
+				new Token(EOF, 0, 15, "")
+		);
+	}
+
+	@Test
+	public void testOperators() {
+		runtest("/ == = >= > <= < - != + *",
+				new Token(DIV, 0, 0, "/"),
+				new Token(EQEQ, 0, 2, "=="),
+				new Token(EQL, 0, 5, "="),
+				new Token(GEQ, 0, 7, ">="),
+				new Token(GT, 0, 10, ">"),
+				new Token(LEQ, 0, 12, "<="),
+				new Token(LT, 0, 15, "<"),
+				new Token(MINUS, 0, 17, "-"),
+				new Token(NEQ, 0, 19, "!="),
+				new Token(PLUS, 0, 22, "+"),
+				new Token(TIMES, 0 ,24, "*"),
+				new Token(EOF, 0, 25, "")
+		);
+	}
+
+	@Test
+	public void testOperatorEdgeCases() {
+		runtest("> =>=",
+				new Token(GT, 0, 0, ">"),
+				new Token(EQL, 0, 2, "="),
+				new Token(GEQ, 0, 3, ">="),
+				new Token(EOF, 0, 5, "")
+		);
+		runtest("===",
+				new Token(EQEQ, 0, 0, "=="),
+				new Token(EQL, 0, 2, "="),
+				new Token(EOF, 0, 3, "")
+		);
+		runtest("====",
+				new Token(EQEQ, 0, 0, "=="),
+				new Token(EQEQ, 0, 2, "=="),
+				new Token(EOF, 0, 4, "")
+		);
+		runtest(">==",
+				new Token(GEQ, 0, 0, ">="),
+				new Token(EQL, 0, 2, "="),
+				new Token(EOF, 0, 3, "")
+		);
+		runtest("!===",
+				new Token(NEQ, 0, 0, "!="),
+				new Token(EQEQ, 0, 2, "=="),
+				new Token(EOF, 0, 4, "")
+		);
+	}
+
+	@Test
+	public void testIdentifiers() {
+		runtest("nhan123_ breaK_a_Leg789",
+				new Token(ID, 0, 0, "nhan123_"),
+				new Token(ID, 0, 9, "breaK_a_Leg789"),
+				new Token(EOF, 0, 23, "")
+		);
+	}
+
+	@Test
+	public void testIdentifierEdgeCases() {
+		runtest("1_nhan",
+				new Token(INT_LITERAL, 0, 0, "1"),
+				null,
+				new Token(ID, 0, 2, "nhan"),
+				new Token(EOF, 0, 6, "")
+		);
+		runtest("_1Nhan",
+				null,
+				new Token(INT_LITERAL, 0, 1, "1"),
+				new Token(ID, 0, 2, "Nhan"),
+				new Token(EOF, 0, 6, "")
+		);
+		runtest("a==b",
+				new Token(ID, 0, 0, "a"),
+				new Token(EQEQ, 0, 1, "=="),
+				new Token(ID, 0, 3, "b"),
+				new Token(EOF, 0, 4, "")
+		);
+	}
+
+	@Test
+	public void testStringLiteralWithDoubleQuote() {
+		runtest("\"\"\"",
+				new Token(STRING_LITERAL, 0, 0, ""),
+				(Token)null,
+				new Token(EOF, 0, 3, ""));
+	}
+
+	@Test
+	public void testStringLiteral() {
+		runtest("\"\\n\"",
+				new Token(STRING_LITERAL, 0, 0, "\\n"),
+				new Token(EOF, 0, 4, ""));
+	}
+
+
 	@Test
 	public void testStringLiteral2() {
 		runtest("\"the\" \"b1g\" brown f0x",
@@ -95,7 +190,7 @@ public class LexerTests {
 				new Token(ID, 0, 18, "f0x"),
 				new Token(EOF, 0, 21, ""));
 	}
-	
+
 	@Test
 	public void testIntegerLiteral() {
 		runtest("0203 405 boolean123 -158 7+7=14",
